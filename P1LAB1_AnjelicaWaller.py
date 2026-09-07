d6831bd2dbac297f2f83b38d4e5f44f29d9f4c42
wallera4523 <wallera4523@student.faytechcc.edu>
2026-09-07 15:46:48 -0400

    Uploaded file

---
+++ P1LAB1_AnjelicaWaller.py
@@ -0,0 +1,8 @@
+# Anjelica Waller
+# P1LAB1_AnjelicaWaller.py
+# input and print
+
+first_name = input("Enter your first name: ") 
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name + "!","Welcome to CTI-110!")
